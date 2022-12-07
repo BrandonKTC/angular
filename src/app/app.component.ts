33102e6cde349b70ab967ab5d75ad96287ce1900
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-07 20:23:17 +0100

    ng Directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,23 @@ import { Component } from "@angular/core";
 	// styles: ['']
 })
 export class AppComponent {
-	name = "Brandon";
+	name = "brandon";
 	imgURL = "https://picsum.photos/id/237/500/500";
+	images = [
+		"https://picsum.photos/237/300",
+		"https://picsum.photos/187/300",
+		"https://picsum.photos/145/300",
+	];
+	currentDate = new Date();
+	cost = 2000;
+	temperature = 25.3;
+	pizza = {
+		toppings: ["pepperoni", "bacon", "mushroom"],
+		size: "large",
+	};
+	blueClass = false;
+	fontSize = 16;
+
 	getName() {
 		return this.name;
 	}
