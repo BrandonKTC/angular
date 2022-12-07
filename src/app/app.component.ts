533da60214cdf9d6fa07fa16593e5b7b1e4697d6
brandon kwamou <b.kwamoutchamou@gmail.com>
2022-12-07 18:11:39 +0100

    Component fundamental and lifecycle method

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  // template: `<p>Hello World!</p>`, // can't use both template and styles at the same time
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  // styles: ['']
+	selector: "app-root",
+	// template: `<p>Hello World!</p>`, // can't use both template and styles at the same time
+	templateUrl: "./app.component.html",
+	styleUrls: ["./app.component.css"],
+	// styles: ['']
 })
-export class AppComponent {}
+export class AppComponent {
+	name = "Brandon";
+	imgURL = "https://picsum.photos/id/237/500/500";
+	getName() {
+		this.name;
+	}
+
+	changeImage(e: KeyboardEvent) {
+		this.imgURL = (e.target as HTMLInputElement).value;
+	}
+
+	logImg(event: string) {
+		console.log(event);
+	}
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import { PostComponent } from "./post/post.component";
 
 @NgModule({
-  imports: [BrowserModule],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+	imports: [BrowserModule],
+	declarations: [AppComponent, PostComponent],
+	bootstrap: [AppComponent],
 })
 export class AppModule {}
